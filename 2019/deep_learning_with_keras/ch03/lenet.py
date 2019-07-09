f3f845edc0f77b526acb1a8362042c3a00e52148
小山紘史 <h-koyama@koyamahiroshishinoiMac.local>
2019-07-10 00:13:29 +0900

    fix bug in lenet.py

--- 2019/deep_learning_with_keras/ch03/lenet.py
+++ 2019/deep_learning_with_keras/ch03/lenet.py
@@ -14,26 +14,26 @@ class MNISTDataset():
         self.image_shape = (28, 28, 1)
         self.num_classes = 10
 
-        def get_batch(self):
-            (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    def get_batch(self):
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+            
+        x_train, x_test = [self.preprocess(d) for d in [x_train, x_test]]
+        y_train, y_test = [self.preprocess(d, label_data=True) for d in [y_train, y_test]]
 
-            x_train, x_test = [self.preprocess(d) for d in [x_train, x_test]]]
-            y_train, y_test = [self.preprocess(d, label_data=True) for d in [y_train, y_test]]]
+        return x_train, y_train, x_test, y_test
 
-            return x_train, y_train, x_test, y_test
+    def preprocess(self, data, label_data=False):
+        if label_data:
+            # convert class vectors to binary class matrices
+            data = keras.utils.to_categorical(data, self.num_classes)
+        else:
+            data = data.astype("float32")
+            data /= 255
+            # add dataset length to top
+            shape = (data.shape[0], ) + self.image_shape
+            data = data.reshape(shape)
 
-        def preprocess(self, data, label_data=False):
-            if label_data:
-                # convert class vectors to binary class matrices
-                data = keras.utils.to_categorical(data, self.num_classes)
-            else:
-                data = data.astype("float32")
-                data /= 255
-                # add dataset length to top
-                shape = (data.shape[0], ) + self.image_shape
-                data = data.reshape(shape)
-
-            return data
+        return data
 
 class Trainer():
 
